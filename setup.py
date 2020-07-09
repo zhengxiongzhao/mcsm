67409adb9b68ef8106d94e1f734586ee09ffb0c0
zxzhao <zhengxiongzhao@gmail.com>
2020-07-10 01:57:46 +0800

    update

--- setup.py
+++ setup.py
@@ -1,29 +1,8 @@
 import setuptools
 import versioneer
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-with open("LICENSE", "r") as fh:
-    license = fh.read()
-
 setuptools.setup(
     name="mcsm_template",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    author="ZhengXiong Zhao",
-    author_email="zhengxiongzhao@gmail.com",
-    description="A small example package",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/mcsm_template",
-    license=license,
-    platforms='any',
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
 )
